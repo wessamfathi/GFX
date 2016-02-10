e248995df8fcfb50cf0a1bdd463a4e5b88203872
Wessam Fathi <wessam@skillplaystudios.com>
2016-02-11 00:43:41 +0100

    Transformations
    Correctly set up rotations, translations, scaling, lookAt, and
    perspective

--- CSE167X/hw2-windows_VS2012/hw2-windows/Transform.cpp
+++ CSE167X/hw2-windows_VS2012/hw2-windows/Transform.cpp
@@ -9,53 +9,69 @@
 // Helper rotation function.  Please implement this.  
 mat3 Transform::rotate(const float degrees, const vec3& axis) 
 {
-    mat3 ret;
-    // YOUR CODE FOR HW2 HERE
-    // Please implement this.  Likely the same as in HW 1.  
-    return ret;
+	const vec3 normaxis = glm::normalize(axis);
+	const float rad = degrees * pi / 180;
+	const auto cosTheta = glm::cos(rad);
+	const auto sinTheta = glm::sin(rad);
+	const mat3 uCross(normaxis.x * normaxis.x, normaxis.x * normaxis.y, normaxis.x * normaxis.z, 
+		normaxis.x * normaxis.y, normaxis.y * normaxis.y, normaxis.y * normaxis.z,
+		normaxis.x * normaxis.z, normaxis.y * normaxis.z, normaxis.z * normaxis.z);
+
+	const mat3 uX(0, normaxis.z, -normaxis.y, -normaxis.z, 0, normaxis.x, normaxis.y, -normaxis.x, 0);
+
+	const mat3 rotation = cosTheta * mat3(1.0) + sinTheta*uX + (1 - cosTheta)*(uCross);
+	return rotation;
 }
 
 void Transform::left(float degrees, vec3& eye, vec3& up) 
 {
-    // YOUR CODE FOR HW2 HERE
-    // Likely the same as in HW 1.  
+	eye = rotate(degrees, up) * eye;
+}
+
+void constructCoordinateFrame(vec3 a, vec3 b, vec3& w, vec3& u, vec3& v)
+{
+	w = glm::normalize(a);
+	u = glm::normalize(glm::cross(b, w));
+	v = glm::cross(w, u);
 }
 
 void Transform::up(float degrees, vec3& eye, vec3& up) 
 {
-    // YOUR CODE FOR HW2 HERE 
-    // Likely the same as in HW 1.  
+	vec3 w, u, v;
+	constructCoordinateFrame(eye, up, w, u, v);
+
+	eye = rotate(-degrees, u) * eye;
+	up = rotate(-degrees, u)*up;
 }
 
 mat4 Transform::lookAt(const vec3 &eye, const vec3 &center, const vec3 &up) 
 {
-    mat4 ret;
-    // YOUR CODE FOR HW2 HERE
-    // Likely the same as in HW 1.  
-    return ret;
+	vec3 w, u, v;
+	constructCoordinateFrame(eye, up, w, u, v);
+
+	mat4 lookAtRotMat(u.x, v.x, w.x, 0, u.y, v.y, w.y, 0, u.z, v.z, w.z, 0, 0, 0, 0, 1);
+	mat4 lookAtTransMat(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, -eye.x, -eye.y, -eye.z, 1);
+
+	return lookAtRotMat * lookAtTransMat;
 }
 
 mat4 Transform::perspective(float fovy, float aspect, float zNear, float zFar)
 {
-    mat4 ret;
-    // YOUR CODE FOR HW2 HERE
-    // New, to implement the perspective transform as well.  
+	fovy = glm::radians(fovy);
+	float f = 1 / tan(fovy / 2);
+	mat4 ret(f / aspect, 0, 0, 0, 0, f, 0, 0, 0, 0, (zFar + zNear) / (zNear - zFar), -1, 0, 0, (2 * zFar*zNear) / (zNear - zFar), 0);
     return ret;
 }
 
 mat4 Transform::scale(const float &sx, const float &sy, const float &sz) 
 {
-    mat4 ret;
-    // YOUR CODE FOR HW2 HERE
-    // Implement scaling 
+	mat4 ret(sx, 0, 0, 0, 0, sy, 0, 0, 0, 0, sz, 0, 0, 0, 0, 1);
     return ret;
 }
 
 mat4 Transform::translate(const float &tx, const float &ty, const float &tz) 
 {
-    mat4 ret;
-    // YOUR CODE FOR HW2 HERE
-    // Implement translation 
+	mat4 ret(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, tx, ty, tz, 1);
     return ret;
 }
 
