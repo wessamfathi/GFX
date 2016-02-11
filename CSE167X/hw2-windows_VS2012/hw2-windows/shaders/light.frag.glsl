a0fdfe5989ec9e5b53f546bbf6cd88a0c07761c5
Wessam Fathi <wessam@skillplaystudios.com>
2016-02-12 00:23:19 +0100

    First iteration of getting the light to work, Upgrade PS to OpenGL 1.3

--- CSE167X/hw2-windows_VS2012/hw2-windows/shaders/light.frag.glsl
+++ CSE167X/hw2-windows_VS2012/hw2-windows/shaders/light.frag.glsl
@@ -1,4 +1,4 @@
-# version 120 
+# version 130 
 
 /* This is the fragment shader for reading in a scene description, including 
    lighting.  Uniform lights are specified from the main program, and used in 
@@ -10,9 +10,9 @@
 // in vec4 myvertex;
 // That is certainly more modern
 
-varying vec4 color;
-varying vec3 mynormal; 
-varying vec4 myvertex; 
+in vec4 color;
+in vec3 mynormal;
+in vec4 myvertex;
 
 const int numLights = 10; 
 uniform bool enablelighting; // are we lighting at all (global).
@@ -31,19 +31,57 @@ uniform vec4 specular;
 uniform vec4 emission; 
 uniform float shininess; 
 
+vec4 ComputeLight (const in vec3 direction, const in vec4 lightcolor, const in vec3 normal, const in vec3 halfvec, const in vec4 mydiffuse, const in vec4 myspecular, const in float myshininess)
+{
+	float nDotL = dot(normal, direction);
+	vec4 lambert = mydiffuse * lightcolor * max (nDotL, 0.0); 
+
+	float nDotH = dot(normal, halfvec);
+	vec4 phong = myspecular * lightcolor * pow (max(nDotH, 0.0), myshininess);
+
+	vec4 retval = lambert + phong;
+	return retval;
+}       
+
 void main (void) 
 {       
-    if (enablelighting) {       
-        vec4 finalcolor; 
+    if (enablelighting)
+	{
+		const vec3 eyepos = vec3(0,0,0);
+		vec4 _mypos = gl_ModelViewMatrix * myvertex;
+		vec3 mypos = _mypos.xyz / _mypos.w;	// Dehomogenize current location 
+		vec3 eyedirn = normalize(eyepos - mypos);
 
-        // YOUR CODE FOR HW 2 HERE
-        // A key part is implementation of the fragment shader
+		// Compute normal, needed for shading. 
+		vec3 normal = normalize(gl_NormalMatrix * mynormal);
 
-        // Color all pixels black for now, remove this in your implementation!
-        finalcolor = vec4(0,0,0,1); 
+		vec4 finalColor = vec4(0, 0, 0, 1);
 
-        gl_FragColor = finalcolor; 
-    } else {
-        gl_FragColor = color; 
+		for (int i = 0; i < numLights; ++i)
+		{
+			if (lightposn[i].w == 0)
+			{
+				// Directional Light
+				vec3 direction0 = normalize (lightposn[i].xyz) ; 
+				vec3 half0 = normalize (direction0 + eyedirn) ; 
+				vec4 result = ComputeLight(direction0, lightcolor[i], normal, half0, diffuse, specular, shininess) ;
+				finalColor += result;
+			}
+			else
+			{
+				// Point Light
+				vec3 position = lightposn[i].xyz / lightposn[i].w ; 
+				vec3 direction1 = normalize (position - mypos) ; // no attenuation 
+				vec3 half1 = normalize (direction1 + eyedirn) ;  
+				vec4 result = ComputeLight(direction1, lightcolor[i], normal, half1, diffuse, specular, shininess) ;
+				finalColor += result;
+			}
+		}
+
+		gl_FragColor = finalColor;
+	}
+	else
+	{
+		gl_FragColor = color; 
     }
 }
