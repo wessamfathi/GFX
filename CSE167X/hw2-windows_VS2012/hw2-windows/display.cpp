0c1716cfef59eadba901d75ce9e3ee87cafedc6a
Wessam Fathi <wessam@skillplaystudios.com>
2016-02-12 00:21:44 +0100

    Whitespaces adjustment

--- CSE167X/hw2-windows_VS2012/hw2-windows/display.cpp
+++ CSE167X/hw2-windows_VS2012/hw2-windows/display.cpp
@@ -25,90 +25,116 @@ using namespace std;
 // Provided for your convenience.  Use is optional.  
 // Some of you may want to use the more modern routines in readfile.cpp 
 // that can also be used.  
-void transformvec (const GLfloat input[4], GLfloat output[4]) 
+void transformvec(const GLfloat input[4], GLfloat output[4])
 {
-  GLfloat modelview[16]; // in column major order
-  glGetFloatv(GL_MODELVIEW_MATRIX, modelview); 
+	GLfloat modelview[16]; // in column major order
+	glGetFloatv(GL_MODELVIEW_MATRIX, modelview);
 
-  for (int i = 0 ; i < 4 ; i++) {
-    output[i] = 0; 
-    for (int j = 0 ; j < 4 ; j++) {
-      output[i] += modelview[4*j+i] * input[j]; 
-    }
-  }
+	for (int i = 0; i < 4; i++)
+	{
+		output[i] = 0;
+		for (int j = 0; j < 4; j++)
+		{
+			output[i] += modelview[4 * j + i] * input[j];
+		}
+	}
 }
 
-void display() 
+void display()
 {
-  glClearColor(0, 0, 1, 0);
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearColor(0.2, 0.2, 0.6, 0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-  // I'm including the basic matrix setup for model view to 
-  // give some sense of how this works.  
+	// I'm including the basic matrix setup for model view to 
+	// give some sense of how this works.  
 
-  glMatrixMode(GL_MODELVIEW);
-  mat4 mv; 
+	glMatrixMode(GL_MODELVIEW);
+	mat4 mv;
 
-  // Either use the built-in lookAt function or the lookAt implemented by the user.
-  if (useGlu) {
-    mv = glm::lookAt(eye,center,up); 
-  } else {
-    mv = Transform::lookAt(eye,center,up); 
-  }
+	// Either use the built-in lookAt function or the lookAt implemented by the user.
+	if (useGlu)
+	{
+		mv = glm::lookAt(eye, center, up);
+	}
+	else
+	{
+		mv = Transform::lookAt(eye, center, up);
+	}
 
-  glLoadMatrixf(&mv[0][0]); 
+	glLoadMatrixf(&mv[0][0]);
 
-  // Lights are transformed by current modelview matrix. 
-  // The shader can't do this globally. 
-  // So we need to do so manually.  
-  if (numused) {
-    glUniform1i(enablelighting,true);
+	// Lights are transformed by current modelview matrix. 
+	// The shader can't do this globally. 
+	// So we need to do so manually.  
+	if (numused)
+	{
+		glUniform1i(enablelighting, true);
 
-    // YOUR CODE FOR HW 2 HERE.  
-    // You need to pass the light positions and colors to the shader. 
-    // glUniform4fv() and similar functions will be useful. See FAQ for help with these functions.
-    // The lightransf[] array in variables.h and transformvec() might also be useful here.
-    // Remember that light positions must be transformed by modelview.  
+		// YOUR CODE FOR HW 2 HERE.  
+		// You need to pass the light positions and colors to the shader. 
+		// glUniform4fv() and similar functions will be useful. See FAQ for help with these functions.
+		// The lightransf[] array in variables.h and transformvec() might also be useful here.
+		// Remember that light positions must be transformed by modelview.  
+		for (int i = 0; i < numused; ++i)
+		{
+			transformvec(lightposn + 4 * i, lightransf + 4 * i);
+		}
 
-  } else {
-    glUniform1i(enablelighting,false); 
-  }
+		glUniform4fv(lightpos, numLights, lightransf);
+		glUniform4fv(lightcol, numLights, lightcolor);
+	}
+	else
+	{
+		glUniform1i(enablelighting, false);
+	}
 
-  // Transformations for objects, involving translation and scaling 
-  mat4 sc(1.0) , tr(1.0), transf(1.0); 
-  sc = Transform::scale(sx,sy,1.0); 
-  tr = Transform::translate(tx,ty,0.0); 
+	// Transformations for objects, involving translation and scaling 
+	mat4 sc(1.0), tr(1.0), transf(1.0);
+	sc = Transform::scale(sx, sy, 1.0);
+	tr = Transform::translate(tx, ty, 0.0);
 
-  // YOUR CODE FOR HW 2 HERE.  
-  // You need to use scale, translate and modelview to 
-  // set up the net transformation matrix for the objects.  
-  // Account for GLM issues, matrix order (!!), etc.  
+	transf = mv * tr * sc;
 
-  glLoadMatrixf(&transf[0][0]); 
+	// YOUR CODE FOR HW 2 HERE.  
+	// You need to use scale, translate and modelview to 
+	// set up the net transformation matrix for the objects.  
+	// Account for GLM issues, matrix order (!!), etc.  
 
-  for (int i = 0 ; i < numobjects ; i++) {
-    object* obj = &(objects[i]); // Grabs an object struct.
+	glLoadMatrixf(&transf[0][0]);
 
-    // YOUR CODE FOR HW 2 HERE. 
-    // Set up the object transformations 
-    // And pass in the appropriate material properties
-    // Again glUniform() related functions will be useful
+	for (int i = 0; i < numobjects; i++)
+	{
+		object* obj = &(objects[i]); // Grabs an object struct.
 
-    // Actually draw the object
-    // We provide the actual glut drawing functions for you.  
-    // Remember that obj->type is notation for accessing struct fields
-    if (obj->type == cube) {
-      glutSolidCube(obj->size); 
-    }
-    else if (obj->type == sphere) {
-      const int tessel = 20; 
-      glutSolidSphere(obj->size, tessel, tessel); 
-    }
-    else if (obj->type == teapot) {
-      glutSolidTeapot(obj->size); 
-    }
+		// YOUR CODE FOR HW 2 HERE. 
+		// Set up the object transformations 
+		
+		
+		// And pass in the appropriate material properties
+		// Again glUniform() related functions will be useful
+		glUniform4fv(ambientcol, 1, obj->ambient);
+		glUniform4fv(diffusecol, 1, obj->diffuse);
+		glUniform4fv(specularcol, 1, obj->specular);
+		glUniform4fv(emissioncol, 1, obj->emission);
+		glUniform1fv(shininesscol, 1, &obj->shininess);
 
-  }
+		// Actually draw the object
+		// We provide the actual glut drawing functions for you.  
+		// Remember that obj->type is notation for accessing struct fields
+		if (obj->type == cube)
+		{
+			glutSolidCube(obj->size);
+		}
+		else if (obj->type == sphere)
+		{
+			const int tessel = 20;
+			glutSolidSphere(obj->size, tessel, tessel);
+		}
+		else if (obj->type == teapot)
+		{
+			glutSolidTeapot(obj->size);
+		}
+	}
 
-  glutSwapBuffers();
+	glutSwapBuffers();
 }
